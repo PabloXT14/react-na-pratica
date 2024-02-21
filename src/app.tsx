0ca5127b9ecbdf8524d1423185fee3e93b32542d
Pablo Alan <pabloxt14@gmail.com>
2024-02-21 20:51:45 -0300

    feat: base page layout

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,12 @@
 export function App() {
-  return <h1 className="">Hello World</h1>
+  return (
+    <div className="space-y-8 py-10">
+      <div>header abas</div>
+
+      <main className="mx-auto max-w-6xl space-y-5">
+        <p>oi</p>
+        <p>oi</p>
+      </main>
+    </div>
+  )
 }
