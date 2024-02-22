a3384fac8653744d4dd2c1c7a7e46e2d0aacac21
Pablo Alan <pabloxt14@gmail.com>
2024-02-21 21:11:30 -0300

    feat: add pre-styled components

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,23 @@
 import { Plus } from 'lucide-react'
+import { Header } from './components/header'
+import { Tabs } from './components/tabs'
+import { Button } from './components/ui/button'
 
 export function App() {
   return (
     <div className="space-y-8 py-10">
-      <div>header abas</div>
+      <div>
+        <Header />
+        <Tabs />
+      </div>
 
       <main className="mx-auto max-w-6xl space-y-5">
         <div className="flex items-center gap-3">
           <h1 className="text-xl font-bold">Tags</h1>
-          <button className="inline-flex items-center gap-1.5 rounded-full bg-teal-300 px-1.5 py-1 text-xs font-medium text-teal-950">
+          <Button variant="primary">
             <Plus className="size-3" />
             Create new
-          </button>
+          </Button>
         </div>
       </main>
     </div>
